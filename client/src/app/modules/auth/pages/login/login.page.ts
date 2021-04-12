74fa19117aadc8754ed69e1a973db0e3cb3d093f
mohammadreza shahbazi <king.mohammadreza@yahoo.com>
2021-04-12 22:08:02 +0430

    ..

--- client/src/app/modules/auth/pages/login/login.page.ts
+++ client/src/app/modules/auth/pages/login/login.page.ts
@@ -7,34 +7,33 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'ag-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage extends LanguageCheckerComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) {
     super();
   }
-  
+
   form = new FormGroup({
     email: new FormControl(null, [Validators.required, Validators.email]),
     password: new FormControl(null, [Validators.required]),
-    rememberMe: new FormControl(null),
+    rememberMe: new FormControl(null)
   });
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
   onSubmit() {
     if (this.form.valid) {
-      this.authService
-        .login({
-          email: this.form.get('email').value,
-          password: this.form.get('password').value,
-        })
-        .subscribe((res: any) => {
-          if (res?.token) {
-            localStorage.setItem('token', res.token);
-            this.router.navigate(['/dashboard']);
-          }
-        });
+      this.authService.login({
+        email: this.form.get('email').value,
+        password: this.form.get('password').value
+      }).subscribe((res: any) => {
+        if (res?.token) {
+          localStorage.setItem('token', res.token);
+          this.router.navigate(['/dashboard']);
+        }
+      });
     }
   }
 }
